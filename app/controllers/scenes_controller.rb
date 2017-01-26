4135f023884635f2383347797a4d718cd3fdfe9e
Stash Dev <stashappdev@gmail.com>
2017-01-25 19:08:05 -0800

    Pagination
    Added pagination via the Kaminari gem

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -2,7 +2,7 @@ class ScenesController < ApplicationController
   before_action :set_scene, only: [:show, :edit, :update]
 
   def index
-    @scenes = Scene.all
+    @scenes = Scene.all.page params[:page]
   end
 
   def show
