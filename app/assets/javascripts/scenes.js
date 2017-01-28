d70fdc33aae1f76cdf6dba9e4d35ef07a335bbe6
Stash Dev <stashappdev@gmail.com>
2017-01-28 14:36:16 -0800

    Added filter view for Scenes

---
+++ app/assets/javascripts/scenes.js
@@ -0,0 +1,43 @@
+$(function() {
+  $('#studio-filter').dropdown({
+    apiSettings: {
+      onResponse: function(res) {
+        var response = { success: true, results: [] };
+        $.each(res, function(index, item) {
+          var name = item.name || 'Unknown', maxResults = 8;
+          if (index >= maxResults) { return false; }
+
+          response.results.push({
+            name: item.name,
+            value: item.id
+          });
+        });
+
+        return response;
+      },
+      url: '/studios.json?q={query}'
+    },
+    forceSelection: false
+  });
+
+  $('#performer-filter').dropdown({
+    apiSettings: {
+      onResponse: function(res) {
+        var response = { success: true, results: [] };
+        $.each(res, function(index, item) {
+          var name = item.name || 'Unknown', maxResults = 8;
+          if (index >= maxResults) { return false; }
+
+          response.results.push({
+            name: item.name,
+            value: item.id
+          });
+        });
+
+        return response;
+      },
+      url: '/performers.json?q={query}'
+    },
+    forceSelection: false
+  });
+});
