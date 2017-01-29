e0421874fed3d9a16cfca83601a040057a5c4649
Stash Dev <stashappdev@gmail.com>
2017-01-29 13:35:38 -0800

    [Fix] Saving search filters
    Search filters are now reloaded from the query string when filtering.  Uses a hack, but it works...

--- app/assets/javascripts/scenes.js
+++ app/assets/javascripts/scenes.js
@@ -1,6 +1,12 @@
+$.urlParam = function (name) {
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results == null) { return 0; }
+  return results[1] || 0;
+}
+
 $(function() {
   $('.ui.dropdown').dropdown();
-  
+
   $('#studio-filter').dropdown({
     apiSettings: {
       onResponse: function(res) {
@@ -41,5 +47,18 @@ $(function() {
       url: '/performers.json?q={query}'
     },
     forceSelection: false
+  });
+
+  // HACK: Fields don't populate automatically.  We click to force the API request so there is something to fill...
+  $('#performer-filter').trigger('click');
+  $('#studio-filter').trigger('click');
+  $('#scene-search').focus();
+  $('form')
+  .form('set values', {
+    q: $.urlParam('q') || '',
+    sort: $.urlParam('sort') || 'path',
+    direction: $.urlParam('direction') || 'desc',
+    filter_studios: decodeURIComponent($.urlParam('filter_studios')).split(','),
+    filter_performers: decodeURIComponent($.urlParam('filter_performers')).split(',')
   });
 });
