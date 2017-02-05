91f74b15eb2e164344d86aebeb62489a5ced90c6
Stash Dev <stashappdev@gmail.com>
2017-02-05 12:40:01 -0800

    [Fix] Dropdowns
    Fill in the dropdowns via template rather than API to fix issues and remove hacks

--- app/assets/javascripts/scenes.js
+++ app/assets/javascripts/scenes.js
@@ -8,53 +8,12 @@ $(function() {
   $('.ui.dropdown').dropdown();
 
   $('#studio-filter').dropdown({
-    apiSettings: {
-      onResponse: function(res) {
-        var response = { success: true, results: [] };
-        $.each(res, function(index, item) {
-          var name = item.name || 'Unknown', maxResults = 8;
-          if (index >= maxResults) { return false; }
-
-          response.results.push({
-            name: item.name,
-            value: item.id
-          });
-        });
-
-        return response;
-      },
-      url: '/studios.json?q={query}'
-    },
     forceSelection: false
   });
 
   $('#performer-filter').dropdown({
-    apiSettings: {
-      onResponse: function(res) {
-        var response = { success: true, results: [] };
-        $.each(res, function(index, item) {
-          var name = item.name || 'Unknown', maxResults = 8;
-          if (index >= maxResults) { return false; }
-
-          response.results.push({
-            name: item.name,
-            value: item.id
-          });
-        });
-
-        return response;
-      },
-      url: '/performers.json?q={query}'
-    },
     forceSelection: false
   });
-
-  if (!window.location.href.includes('/edit')) {
-    // HACK: Fields don't populate automatically.  We click to force the API request so there is something to fill...
-    $('#performer-filter').trigger('click');
-    $('#studio-filter').trigger('click');
-    $('#scene-search').focus();
-  }
 
   var q = $.urlParam('q').replace(/\+/g, '%20');
   q = q.replace(/\+/g, '%20');
