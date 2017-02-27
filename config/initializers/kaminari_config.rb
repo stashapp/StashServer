2a8f60eebb2181228f6282d161f0b37179a66273
Stash Dev <stashappdev@gmail.com>
2017-02-26 18:40:20 -0800

    [Project] Increased page size

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Kaminari.configure do |config|
-  config.default_per_page = 21
+  config.default_per_page = 72
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
