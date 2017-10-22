bf678bbfb5a58b23a4e0fdb7eaf9fc588248929b
Stash Dev <stashappdev@gmail.com>
2017-10-22 15:42:03 -0700

    Bug fix for the same column name

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -5,7 +5,7 @@ module Sortable
     def sortable params, default:
       sort_direction = %w[asc desc].include?(params[:direction]) ?  params[:direction] : 'asc'
       sort_column = self.column_names.include?(params[:sort]) ? params[:sort] : default
-      reorder(sort_column + ' ' + sort_direction)
+      reorder("#{table_name}.#{sort_column}" + ' ' + sort_direction)
     end
   end
 end
